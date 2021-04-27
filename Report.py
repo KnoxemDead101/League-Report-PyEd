af04f410bc845f405ef129c1af6e9dcba63b0d74
knoxemdead101 <leighton.C.knox3@gmail.com>
2021-04-26 23:02:01 -0700

    Added getting the summoner rank in previous season as well as their current champion mastery

--- Report.py
+++ Report.py
@@ -3,32 +3,36 @@ import keys
 kassadin.set_riot_api_key(keys.dank_keys())
 kassadin.set_default_region("NA")
 name = input("Enter first and last name ")
-userName = input("What is your username ")
+userName = input("What is your username in game")
 summoner = kassadin.get_summoner(name=userName)
 level = summoner.level
+rank_last_season = summoner.rank_last_season
+this_mans_champion_masteries = summoner.champion_masteries
 favoriteRoles = []
 while True:
-    roles = input("List favorite roles ")
+    roles = input("List your favorite roles to play")
     if roles == "Q":
         break
     else:
         favoriteRoles.append(roles)
 favoriteChampions = []
 while True:
-    champions = input("List favorite champions ")
+    champions = input("List your favorite champions to play as of late (just to make your profile up to date)")
     if champions == "Q":
         break
     else:
         favoriteChampions.append(champions)
 
 
-def player_report(player_name, user_name, player_level, player_role, player_favorite_champions):
+def player_report(player_name, user_name, player_level, player_role, player_favorite_champions, player_rank_last_season, this_mans_champion_masteries):
     print("=== LEAGUE PLAYER REPORT ===")
     print(f"Full name is {player_name}")
     print(f"Username is {user_name}")
     print(f"Level is {player_level}")
     print(player_role)
     print(player_favorite_champions)
+    print(player_rank_last_season)
+    print(this_mans_champion_masteries)
 
 
-player_report(name, userName, level, favoriteRoles, favoriteChampions)
+player_report(name, userName, level, favoriteRoles, favoriteChampions, rank_last_season, this_mans_champion_masteries)
