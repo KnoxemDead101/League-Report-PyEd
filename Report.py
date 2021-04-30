40be556443fc93fff53027c784d1e4eb689c7d96
knoxemdead101 <leighton.C.knox3@gmail.com>
2021-04-30 15:23:47 -0700

    Variable renaming, method renaming, also included what I think prints out a players match history. Also found something called current match haven't tested it so its in #s

--- Report.py
+++ Report.py
@@ -1,5 +1,6 @@
 import cassiopeia as kassadin
 import keys
+
 kassadin.set_riot_api_key(keys.dank_keys())
 kassadin.set_default_region("NA")
 name = input("Enter first and last name ")
@@ -7,7 +8,10 @@ userName = input("What is your username in game")
 summoner = kassadin.get_summoner(name=userName)
 level = summoner.level
 rank_last_season = summoner.rank_last_season
-this_mans_champion_masteries = summoner.champion_masteries
+champion_masteries = summoner.champion_masteries
+match_history = summoner.match_history
+# This is a command and i have no idea what this can lead to now
+# current_match = summoner.current_match
 favoriteRoles = []
 while True:
     roles = input("List your favorite roles to play")
@@ -24,7 +28,8 @@ while True:
         favoriteChampions.append(champions)
 
 
-def player_report(player_name, user_name, player_level, player_role, player_favorite_champions, player_rank_last_season, this_mans_champion_masteries):
+def display_player_info(player_name, user_name, player_level, player_role, player_favorite_champions,
+                        player_rank_last_season, champion_mastery, match_histroy):
     print("=== LEAGUE PLAYER REPORT ===")
     print(f"Full name is {player_name}")
     print(f"Username is {user_name}")
@@ -32,7 +37,9 @@ def player_report(player_name, user_name, player_level, player_role, player_favo
     print(player_role)
     print(player_favorite_champions)
     print(player_rank_last_season)
-    print(this_mans_champion_masteries)
+    print(champion_mastery)
+    print(match_histroy)
 
 
-player_report(name, userName, level, favoriteRoles, favoriteChampions, rank_last_season, this_mans_champion_masteries)
+display_player_info(name, userName, level, favoriteRoles, favoriteChampions, rank_last_season,
+                    champion_masteries, match_history)
